37d6a08b24ea5cbd356bec12601b7f4151f02e60
Søren Louv-Jansen <sorenlouv@gmail.com>
2017-07-04 15:14:34 +0200

    Refactor graph utils

--- wash-client/src/App.js
+++ wash-client/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   LineChart,
+  Legend,
   Line,
   XAxis,
   YAxis,
@@ -17,59 +18,67 @@ import {
 
 import './App.css';
 
-const data = getByDayOfMonth(sampleData);
+const data = getByHoursOfDay(sampleData);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LineChart
-          width={800}
-          height={400}
-          data={data}
-          margin={{ top: 5, right: 30, left: 20, bottom: 20 }}
-        >
-          <XAxis
-            label={{
-              value: 'Tid',
-              position: 'insideBottom',
-              dy: 15,
-              fill: '#888'
-            }}
-            dataKey="key"
-            tickFormatter={key => {
-              return key;
-            }}
-          />
-          <YAxis
-            domain={['dataMin - 1', 'dataMax + 1']}
-            label={{
-              value: 'Antal ledige',
-              position: 'insideLeft',
-              angle: -90,
-              dy: 20,
-              fill: '#888'
-            }}
-          />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
+function App() {
+  return (
+    <div className="App">
+      <LineChart
+        width={800}
+        height={400}
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 20 }}
+      >
+        <XAxis
+          label={{
+            value: 'Tid',
+            position: 'insideBottom',
+            dy: 15,
+            fill: '#888'
+          }}
+          dataKey="label"
+        />
+        <YAxis
+          domain={['dataMin - 1', 'dataMax + 1']}
+          label={{
+            value: 'Antal ledige',
+            position: 'insideLeft',
+            angle: -90,
+            dy: 20,
+            fill: '#888'
+          }}
+        />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Legend
+          verticalAlign="top"
+          onClick={() => {
+            debugger;
+          }}
+        />
 
-          <Line
-            type="monotone"
-            dataKey="washersAvg"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-          <Line
-            type="monotone"
-            dataKey="dryersAvg"
-            stroke="#82ca9d"
-            label="Tørretumblere"
-          />
-        </LineChart>
-      </div>
-    );
-  }
+        <Line
+          name="Vaskemaskiner"
+          type="monotone"
+          dataKey="washersAvg"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line
+          name="Tørretumblere"
+          type="monotone"
+          dataKey="dryersAvg"
+          stroke="#82ca9d"
+        />
+        <Line
+          name="Bad"
+          type="monotone"
+          dataKey="showersAvg"
+          stroke="#ff0000"
+        />
+      </LineChart>
+    </div>
+  );
 }
 
 export default App;
--- wash-client/src/utils.js
+++ wash-client/src/utils.js
@@ -2,45 +2,39 @@ import _ from 'lodash';
 
 // By day of week
 export function getByDayOfWeek(items) {
-  const obj = items.reduce((memo, item) => {
-    const weekDay = new Date(item.time).getDay();
-    return getSmth(memo, item, weekDay);
-  }, {});
-
-  return getAsSortedArray(obj);
+  return getChartData(items, item => {
+    const key = new Date(item.time).getDay();
+    const label = key;
+    return { key, label };
+  });
 }
 
 // by hour of day
 export function getByHoursOfDay(items) {
-  const obj = items.reduce((memo, item) => {
-    const hour = new Date(item.time).getHours();
-    return getSmth(memo, item, hour);
-  }, {});
-
-  return getAsSortedArray(obj);
+  return getChartData(items, item => {
+    const key = new Date(item.time).getHours();
+    const label = key;
+    return { key, label };
+  });
 }
 
 export function getByDayOfMonth(items) {
-  const obj = items.reduce((memo, item) => {
-    const day = new Date(item.time).getDate();
-    return getSmth(memo, item, day);
-  }, {});
-
-  return getAsSortedArray(obj);
+  return getChartData(items, item => {
+    const key = new Date(item.time).getDate();
+    const label = key;
+    return { key, label };
+  });
 }
 
 // by hour of week
 export function getByHourOfWeek(items) {
-  const obj = items.reduce((memo, item) => {
+  return getChartData(items, item => {
     const weekDay = new Date(item.time).getDay();
     const hour = new Date(item.time).getHours();
     const key = parseInt(`${weekDay}${_.padStart(hour, 2, '0')}`, 10);
     const label = `${getWeekdayLabel(weekDay)}, kl. ${hour}`;
-
-    return getSmth(memo, item, key, label);
-  }, {});
-
-  return getAsSortedArray(obj);
+    return { key, label };
+  });
 }
 
 function getWeekdayLabel(i) {
@@ -55,39 +49,38 @@ function getWeekdayLabel(i) {
   ][i];
 }
 
+function getChartData(items, iteratee) {
+  const chartData = items.reduce((state, item) => {
+    const { key, label } = iteratee(item);
+
+    return {
+      ...state,
+      [key]: {
+        ...state[key],
+        key,
+        label,
+        washers: _.get(state, `[${key}].washers`, 0) + item.washers.available,
+        dryers: _.get(state, `[${key}].dryers`, 0) + item.dryers.available,
+        showers: _.get(state, `[${key}].showers`, 0) + item.showers.available,
+        occurrences: _.get(state, `[${key}].occurrences`, 0) + 1
+      }
+    };
+  }, {});
+
+  return Object.values(chartData).sort(sortByKey).map(getAverages);
+}
+
+function getAverages(item) {
+  return {
+    ...item,
+    washersAvg: round(item.washers / item.occurrences),
+    dryersAvg: round(item.dryers / item.occurrences),
+    showersAvg: round(item.showers / item.occurrences)
+  };
+}
+
 function sortByKey(a, b) {
   return a.key - b.key;
-}
-
-function getAsSortedArray(obj) {
-  return Object.values(obj).sort(sortByKey);
-}
-
-function getSmth(memo, item, key, label) {
-  if (!memo[key]) {
-    memo[key] = {
-      key,
-      label,
-      washers: 0,
-      washersAvg: 0,
-      dryers: 0,
-      dryersAvg: 0,
-      showers: 0,
-      showersAvg: 0,
-      items: 0
-    };
-  }
-
-  memo[key].washers += item.washers.available;
-  memo[key].dryers += item.dryers.available;
-  memo[key].showers += item.showers.available;
-  memo[key].items += 1;
-
-  memo[key].washersAvg = round(memo[key].washers / memo[key].items);
-  memo[key].dryersAvg = round(memo[key].dryers / memo[key].items);
-  memo[key].showersAvg = round(memo[key].showers / memo[key].items);
-
-  return memo;
 }
 
 function round(num) {
